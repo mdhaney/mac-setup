f1832e7a57ff0e988bd92983cf91013c30bc6d26
Mike Haney <txmikester@gmail.com>
2017-11-30 12:11:38 -0600

    rework bootstrap

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # mac-setup
 ## Intro
-Scripts and notes for bootstrapping a new Mac
+Scripts and notes for bootstrapping a new Mac with multiple accounts (I use separate accounts for work, personal, consulting, etc.).  
 
 ## Bootstrap
-Start with a clean install of OSX and go through the installation wizard as usual (set iCloud ID, etc.).  Once you are at the desktop, open a terminal and change to the /Users/Shared folder, then clone this repo.  Install the command-line tools when prompted.
+Start with a clean install of OSX and go through the installation wizard as usual (set iCloud ID, etc.) for one of your accounts.  Then download the bootstrap script and execute it from the command line.  This does the following:
+- installs the Xcode command-line tools (needed by just about everything else)
+- installs ansible
+- creates a 'brew' group and installs homebrew
+- adjusts permissions so any user added to the brew group will be able to manage homebrew (all users can use brew packages, just not install or update)
+- clones the repo and runs Ansible for the current user
 
+## Configuring users
+
+TODO
+
+
+## Documentation
+
+TODO - document vars and override values
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,28 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 #install command-line tools
-#xcode-select --install
+xcode-select --install
+
+#create brew group
+sudo dseditgroup -o create -n /Local/Default -r "Homebrew" brew
+sudo dseditgroup -o edit -n /Local/Default -a $(whoami) -t user brew
 
 # install homebrew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#install ansible
+# change homebrew permissions
+sudo chgrp -R brew /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/sbin /usr/local/share /usr/local/var /usr/local/Frameworks /usr/local/share/man /usr/local/opt
+sudo chmod -R g+w /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/sbin /usr/local/share /usr/local/var /usr/local/Frameworks /usr/local/share/man /usr/local/opt
+
+# install ansible
 sudo easy_install pip
 sudo pip install ansible
+
+# clone the config repo
+git clone https://github.com/mdhaney/mac-setup.git /Users/Shared/mac-setup
+
+# run config
+/Users/Shared/mac-setup/config.sh
+
+
+

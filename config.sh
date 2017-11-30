7bc1fbeb0d73dd2540c79af1ef59127d471c877d
Mike Haney <txmikester@gmail.com>
2017-11-30 09:34:25 -0600

    test simple ansible script

--- config.sh
+++ config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #install ansible roles
-ansible-galaxy install -r requirements.yml
+#ansible-galaxy install -r requirements.yml
 
 # run ansible
-ansible-playbook main.yml -i inventory -K
+ansible-playbook main.yml -i inventory
+
+# use if you want to pre-prompt for sudo password
+#ansible-playbook main.yml -i inventory -K

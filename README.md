b40f32716a250d41b3e93cc0f5bba7b10627ea79
Mike Haney <txmikester@gmail.com>
2017-11-30 09:40:25 -0600

    remove command-line tools install

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#mac-setup
+# mac-setup
+## Intro
+Scripts and notes for bootstrapping a new Mac
+
+## Bootstrap
+Start with a clean install of OSX and go through the installation wizard as usual (set iCloud ID, etc.).  Once you are at the desktop, open a terminal and change to the /Users/Shared folder, then clone this repo.  Install the command-line tools when prompted.
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #xcode-select --install
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #install ansible
 sudo easy_install pip
